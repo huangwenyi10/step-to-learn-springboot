93f2c88b1455537b1f0676e66d07edc8d0359b18
Ay <wenyi.huang@e-vada.com>
2018-01-03 21:50:41 +0800

    md

---
+++ src/main/java/com/example/demo/StepToLearnSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepToLearnSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepToLearnSpringbootApplication.class, args);
+	}
+}
